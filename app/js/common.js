f1e036b08a447a2c61791b79d2093a29d61a245a
Viktoria <zaharova181114@gmail.com>
2022-10-29 15:05:43 +0300

    add loader

--- app/js/common.js
+++ app/js/common.js
@@ -75,3 +75,75 @@ $('.go_to').click(function (e) {
     return false;
 });
 
+
+// show list
+$('.toggle-requisites').on('click', function (e) {
+    e.preventDefault();
+
+    var
+        $this = $(this),
+        content = $(this).parent().find('.list-requisites');
+
+
+    if (!$this.hasClass('trigger')) {
+        $this.addClass('trigger');
+        $this.html('Скрыть реквизиты');
+
+        content.slideDown();
+    } else {
+        $this.removeClass('trigger');
+        $this.html('Показать реквизиты');
+
+        content.slideUp();
+    }
+});
+// show list
+
+
+$('.information-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
+
+//auto counter total//
+let homeSlider = $('.information-slider');
+
+$('.counter-slide__default').text("/ " + homeSlider.slick("getSlick").slideCount);
+
+homeSlider.on('afterChange', function (event, slick, currentSlide) {
+    $(".counter-slide__cp").text(currentSlide < 10 ? `${currentSlide + 1}` : currentSlide + 1);
+});
+
+
+// timer
+
+var timer = setInterval(timerFunction, 1000),
+    time = 59,
+    procent = 100 / time,
+    per = 0;
+const timerBlock = document.querySelector(".timer");
+
+function timerFunction() {
+    per += procent;
+    //Hour
+    hour = Math.floor(time / 3600);
+    hour = hour < 10 ? "0" + hour : hour;
+    //Minutes
+    minute = Math.floor((time - hour * 3600) / 60);
+    minute = minute < 10 ? "0" + minute : minute;
+    minute = minute > 59 ? "00" : minute;
+    //Seconds
+    second = time % 60;
+    second = second < 10 ? + second : second;
+
+    timerBlock.innerHTML = second + ' ' + 'сек';
+    time--;
+    // timerBlock.style.cssText =
+    //     "background: conic-gradient(hsla(180, 100%, 50%, 1) " + per +
+    //     "%, transparent 0 " + (100 - per) + "%)";
+
+    if (time === -1) clearInterval(timer);
+}
+
