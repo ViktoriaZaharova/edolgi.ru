86eb595025b795ddf73e4032cf5cb4893a89b7c5
Виктория Захарова <zaharova181114@gmail.com>
2023-05-26 12:42:21 +0300

    1

--- js/common.js
+++ js/common.js
@@ -211,6 +211,15 @@ $(document).ready(function () {
 	});
 });
 
+$(".bailiff-next-step").click(function (e) {
+	e.preventDefault();
+	var id = $(this).attr('data-tab'),
+		content = $('.form-bailiff .js-tab-content[data-tab="' + id + '"]');
+
+	$('.form-bailiff .js-tab-content.active').removeClass('active'); // 3
+	content.addClass('active'); // 4
+});
+
 $('.autopayment-settings-toggle').on('click', function (e) {
 	e.preventDefault();
 	$(this).toggleClass('active').parents().find('.block-hidden').fadeToggle();
@@ -247,3 +256,53 @@ $('.menu li a').each(function () {
 	}
 });
 // end
+
+
+$(function () {
+	var next = $('.next-step');
+	var prev = $('.prev-step');
+	var progress = $('.progress-bg');
+	var stepsCount = $('.bailiff-item').length;
+	var currentStep = 1;
+
+	function calculate(current, total) { // Функция для подсчета текущего %
+		return (current / total) * 100
+	}
+
+	$('.next-step-first').click(function () {
+		$('.section-loader-bailiff').fadeIn();
+		$('.progress-wrapper').fadeIn();
+	});
+	
+	setTimeout(function () {
+		$('.section-loader-bailiff').fadeOut();
+	}, 60000);
+
+	setTimeout(function () {
+		$('.bailiff-item-5.active').find('.list-result-information li').removeClass('load-result');
+	}, 30000);
+
+	setTimeout(function () {
+		$('.bailiff-item-5.active').removeClass('active').next('.bailiff-item').addClass('active');
+	}, 31000);
+
+	progress.width(calculate(currentStep, stepsCount) + '%'); // устанавливаем начальную ширину полосы
+	next.on('click', function () {
+		// var currentSection = $(this).parents('.bailiff-item');
+		progress.width(calculate(++currentStep, stepsCount) + '%');
+		$('.progress-steps__cp').text(currentStep); // устанавливаем ширину полосы после изменения шага
+		$('.progress-steps__default').text(stepsCount);
+	});
+
+	prev.on('click', function () {
+		// var currentSection = $(this).parents('.bailiff-item');
+		// currentSection.removeClass('active');
+		// currentSection.next().addClass('active');
+		progress.width(calculate(--currentStep, stepsCount) + '%'); // устанавливаем ширину полосы после изменения шага
+		$('.progress-steps__cp').text(currentStep);
+		$('.progress-steps__default').text(stepsCount);
+		if ($('.next-step-first').parents('.bailiff-item').hasClass('active')) {
+			$('.progress-wrapper').hide();
+		}
+	});
+});
